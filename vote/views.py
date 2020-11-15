4cef39025d384e84d9c676e80a83a81187e841cc
taziamoma <taziamoma@gmail.com>
2020-11-15 15:23:37 -0600

    Finished conversion of template to Django

--- vote/views.py
+++ vote/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
+
+
+def vote(request):
+    return render(request, 'vote.html', {})
